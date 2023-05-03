37d6d91c1ab392cd608a5a03548057eb995d7621
mors-e <mors@nemors.ru>
2023-05-03 19:17:26 +0300

    laba 10

--- express/api/middlewares/middleware.js
+++ express/api/middlewares/middleware.js
@@ -1,8 +1,8 @@
 import helmet from 'helmet';
 import morgan from 'morgan';
 import {getApiKey} from "../../services/service.js";
+import swaggerJsDoc from "swagger-jsdoc";
 
-const error = new Error("Ошибка")
 export const myHelmet = helmet()
 export const myMorgan = morgan('dev')
 
@@ -14,41 +14,63 @@ export function validateInput(req, res, next)  {
     next();
 }
 
-export async function authorizationApi(req, res, next){
-<<<<<<< HEAD
-    try {
-        const keys = await getApiKey()
-        if (keys) {
-            if (!keys.includes(req.headers["apikey"]) && req.method !== "GET" && req.url !== "/login") {
-                return res.status(400).send('access denied')
-            }
-            else{
-                next()
-            }
-        }else{
-            res.send('not apikey in databases')
-        }
-    }catch (e){next(e)}
-=======
+export async function authorizationApi(req, res, next) {
     const keys = await getApiKey()
     if (keys) {
-        if (keys.includes(req.headers["apikey"]) && req.method !== "GET" && req.url !== "/login") {
+        if (!keys.includes(req.headers["apikey"]) && req.method !== "GET" && req.url !== "/login") {
             return res.status(403).send('access denied')
-        }
-        else{
+        } else {
             next()
         }
-    }else{
+    } else {
         res.send('not apikey in databases')
     }
->>>>>>> 592e23ef2ddb6c0baff2ba2e91407fe68289283c
 }
 
 export function badURL(req, res){
-    res.send(400,"Такой страницы не существует")
+    res.status(400).send("Такой страницы не существует")
 }
 
 export async function err(err, req, res, next){
     err.status = 500
     res.status(err.status).send(err.message)
 }
+
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info:{
+            title: "Documentations",
+            version: "1.0.0",
+            contact: {
+                name: "llega",
+            },
+        },
+        servers: [
+            {
+                url: `http://127.0.0.1:8000/api/v3`
+            },
+        ],
+        tags:[
+            {
+                name: "API",
+                description: "create and delete apikey",
+            },
+            {
+                name: "Models",
+                description: "CRUD in models",
+            },
+            {
+                name: "Comments",
+                description: "CRUD in comments",
+            },
+            {
+                name: "Home",
+                description: "Home page",
+            },
+        ],
+    },
+    apis: ['api/v3/documentations.yaml']
+}
+
+export const swaggerDocs = swaggerJsDoc(swaggerOptions)
--- express/app.js
+++ express/app.js
@@ -2,7 +2,8 @@ import handlers from './api/v3/api.js';
 import express from 'express';
 import path from 'path';
 import bodyParser from "body-parser";
-import {authorizationApi, badURL, myHelmet, myMorgan, err} from "./api/middlewares/middleware.js";
+import swaggerUI from "swagger-ui-express"
+import {authorizationApi, badURL, myHelmet, myMorgan, err, swaggerDocs} from "./api/middlewares/middleware.js";
 
 const app = express()
 
@@ -21,6 +22,8 @@ app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, 'public')))
 
 app.use('/api/v3', authorizationApi, handlers)
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 app.use(badURL)
 app.use(err)
